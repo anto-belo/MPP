1ddf117d4f30e70fee10dfd6de82df8ac726e92f
anto-belo <antony.belousov02@gmail.com>
2021-10-03 15:53:55 +0300

    MPP-8 completed

---
+++ MPP-8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Test;
+
+namespace MPP_8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string assemblyPath = "G:\\Мой диск\\СПП\\MPP\\Test\\bin\\Debug\\Test.exe";
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            Console.WriteLine(assembly.FullName);
+
+            Type[] types = assembly.GetTypes();
+            foreach (Type t in types)
+            {
+                if (Attribute.GetCustomAttribute(t, typeof(ExportClassAttribute)) != null)
+                {
+                    Console.WriteLine(t.FullName);
+                }
+            }
+        }
+    }
+}
