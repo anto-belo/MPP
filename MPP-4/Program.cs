7030b948efc0e7dfa44d631bd8153fc718eb4ccc
anto-belo <antony.belousov02@gmail.com>
2021-10-12 22:41:59 +0300

    MPP-4 completed

---
+++ MPP-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MPP_4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Beep();
+            Process[] processes = Process.GetProcessesByName("GitHubDesktop");
+            // ProcessThreadCollection processThreads = proc.Threads;
+ 
+            foreach(Process process in processes)
+            {
+                Console.WriteLine($"Handle: {process.Handle} ID: {process.Id}  Name: {process.ProcessName}");
+            }
+            // foreach (Process process in Process.GetProcesses())
+            // {
+            //     Console.WriteLine($"ID: {process.Id}  Name: {process.ProcessName}");
+            // }
+        }
+    }
+}
