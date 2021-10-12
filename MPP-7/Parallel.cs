e4ef21a845ffe3dd9b5a4b153d2a37685c242e00
anto-belo <antony.belousov02@gmail.com>
2021-10-13 01:01:11 +0300

    MPP-7 completed

---
+++ MPP-7/Parallel.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+
+namespace MPP_7
+{
+    public class Parallel
+    {
+        public static void WaitAll(WaitCallback[] tasks)
+        {
+            foreach (var task in tasks)
+            {
+                ThreadPool.QueueUserWorkItem(task);
+            }
+
+            bool readyToStop;
+            do
+            {
+                ThreadPool.GetMaxThreads(out int mWorkers, out int mIoThreads);
+                ThreadPool.GetAvailableThreads(out int aWorkers, out int aIoThreads);
+                readyToStop = mWorkers == aWorkers;
+            } while (!readyToStop);
+        }
+    }
+}
---
+++ MPP-7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace MPP_7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tasks = CreateTasks();
+            Parallel.WaitAll(tasks);
+        }
+
+        private static WaitCallback[] CreateTasks()
+        {
+            WaitCallback[] tasks = new WaitCallback[200];
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                int index = i;
+                tasks[i] = _ =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Task {index} completed.");
+                };
+            }
+
+            return tasks;
+        }
+    }
+}
